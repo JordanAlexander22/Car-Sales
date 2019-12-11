98730c11dcf518e301cb40c55628699faf21087f
jordan <jordan@jordans-Air.lan>
2019-12-11 23:18:41 +0700

    added features but recieving error

---
+++ src/state/actionCreator.js
@@ -0,0 +1,11 @@
+import * as types from "./actionTypes";
+export function addFeature() {
+  return {
+    type: types.ADD_FEATURE
+  };
+}
+export function removeFeature() {
+  return {
+    type: types.REMOVE_FEATURE
+  };
+}
